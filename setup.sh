a6c6e7abca6196ba8037b06ce197eeb55d88b99a
Greg Brimble <developer@gregbrimble.com>
2017-09-01 19:03:47 +0100

    Add upstream to forked repos

--- setup.sh
+++ setup.sh
@@ -7,3 +7,5 @@ pip install -r requirements.txt
 deactivate
 mkdir tmp
 touch tmp/restart.txt
+git remote rm upstream || :
+git remote add upstream git@github.com:GregBrimble/boilerplate-web-service.git
