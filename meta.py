93649f949e464e87f34e72cb0ccd9eb875e7b754
Greg Brimble <developer@gregbrimble.com>
2017-09-02 17:27:53 +0100

    Add GitHub Webhooks autodeployment

--- meta.py
+++ meta.py
@@ -1,10 +1,12 @@
 import datetime
+import hmac
 import json
 import os
 
-from flask import Blueprint
+from flask import abort, Blueprint, request
 
 from upgrader import build, update
+from config import config
 
 meta = Blueprint('meta', __name__)
 
@@ -38,3 +40,42 @@ def queueUpdate():
 def queueBuild():
     threading.Thread(target=build).start()
     return "Build queued. Please note this takes many minutes to complete. Get yourself some tea. You can see when the last complete update was at GET /meta."
+
+
+def verifyGitHubHook(request):
+    header_signature = request.headers.get("X-Hub-Signature")
+    if header_signature is None:
+        abort(403)
+
+    secret = config.get("github_hook", "secret")
+
+    sha_name, signature = header_signature.split("=")
+    mac = hmac.new(str(secret), msg=request.data, digestmod=str(sha_name))
+
+    if str(mac.hexdigest()) != str(signature):
+        abort(403)
+    else:
+        try:
+            return request.get_json()
+        except:     # TODO: Be more specific
+            abort(400)
+
+
+@meta.route("/github_hook", methods=["POST"])
+def incomingGitHubHook():
+    payload = verifyGitHubHook(request)
+
+    if request.headers.get("X-GitHub-Event") != "push":
+        abort(501)
+
+    buildRequired = False
+
+    commits = payload['commits']
+    for commit in commits:
+        if "requirements.txt" in commit['modified']:
+            buildRequired = True
+
+    if buildRequired:
+        return queueBuild()
+    else:
+        return queueUpdate()
