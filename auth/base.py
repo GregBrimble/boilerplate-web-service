6e57bd6b9c2b22d69767ff4f726c77febb9ec66f
Greg Brimble <developer@gregbrimble.com>
2017-11-25 21:57:02 +0000

    Fix base

--- auth/base.py
+++ auth/base.py
@@ -7,24 +7,16 @@ class Protection:
 
     # Exemption from `authentication()`
     def login_exempt(self, f):
-        @wraps(f)
-        def with_exemption(*args, **kwargs):
-            if getattr(f, 'login_required', False):
-                logging.error("Both @login_exempt and @login_required on %s. Defaulting to @login_required..." % str(f))
-                f.login_exempt = False
-            else:
-                f.login_exempt = True
-            return f
-        return with_exemption
+        if getattr(f, 'login_required', False):
+            f.login_exempt = False
+        else:
+            f.login_exempt = True
+        return f
 
     # Requiring login in absence of `authentication()`
     def login_required(self, f):
-        @wraps(f)
-        def with_requirement(*args, **kwargs):
-            if getattr(f, 'login_exempt', False):
-                logging.error("Both @login_exempt and @login_required on %s. Defaulting to @login_required..." % str(f))
-                f.login_exempt = False
+        if getattr(f, 'login_exempt', False):
+            f.login_exempt = False
 
-            f.login_required = True
-            return f
-        return with_requirement
+        f.login_required = True
+        return f
